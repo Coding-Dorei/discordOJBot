a380a9c757b41390a733033cf53fb68a5bd3921c
Coding-Dorei <96749184+Coding-Dorei@users.noreply.github.com>
2022-06-30 03:48:02 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -52,7 +52,7 @@ async def submit(ctx,week,num,path):
         return
     try:
         #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=option)
-        driver = webdriver.Chrome('./chromedriver')
+        driver = webdriver.Chrome(executable_path='./chromedriver')
         driver.get(url)
         driver.find_element(By.ID,"id").send_keys(id)
         driver.find_element(By.ID,"password").send_keys(password,Keys.ENTER)
